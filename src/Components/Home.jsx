6a2a009767ad2d3119562a3a629636059d99b917
John Hunter Skolnick <jhskolnick100@gmail.com>
2024-05-29 11:13:33 -0400

    first push

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import "../Styles/Home.css"
 import toofer from "../assets/toofer.jpg";
 import deer from "../assets/deerfrisco.jpg";
@@ -7,27 +7,36 @@ import woody from "../assets/woody.png";
 import babyMomma from "../assets/babymomma.png";
 import emptyperfection from "../assets/Emptyperfection.jpg";
 import ducksama from "../assets/ducksamapinladin.png"
-import Footer from "./Footer";
 
 
 
-export default function Home(){
 
-    return(
+export default function Home() {
+
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownVisible(!dropdownVisible);
+    }
+
+    return (
         <div>
             <div className="hero">
-                <img src={toofer} className="toofer" alt="To"/>
-                
+                <img src={toofer} className="toofer" alt="To" />
+
             </div>
 
             <div className="below-hero">
                 <h1 className="blog-title">Tales from the Field</h1>
-                <div className="blogcard-container">
-                    <img src={deer} className="interchange-posts" alt="post-1"/>
-                    <img src={turkey} className="interchange-posts" alt="post-2"/>
-                    <img src={woody} className="interchange-posts" alt="post-3"/>
-                    <img src={babyMomma} className="interchange-posts" alt="post-3"/>
-                    <img src={emptyperfection} className="interchange-posts" alt="post-3"/>
+                <button className="dropdown-btn" onClick={toggleDropdown}>
+                    View Photos
+                </button>
+                <div className={`blogcard-container ${dropdownVisible ? "show" : ""}`}>
+                    <img src={deer} className="interchange-posts" alt="post-1" />
+                    <img src={turkey} className="interchange-posts" alt="post-2" />
+                    <img src={woody} className="interchange-posts" alt="post-3" />
+                    <img src={babyMomma} className="interchange-posts" alt="post-3" />
+                    <img src={emptyperfection} className="interchange-posts" alt="post-3" />
                 </div>
                 <div className="midsection">
                     <div className="about">
@@ -35,22 +44,22 @@ export default function Home(){
                         <p>We are forming a brand based upon the good times around the campfire, telling the tales of our hunt from the day or the amazing surf session we had.  Our product will be created from these campfire tales.  We hope to have you all around that campfire soon!  </p>
                     </div>
                     <div className="vid-section">
-                        <img src={ducksama} className="vid"/>
+                        <img src={ducksama} className="vid" />
                     </div>
                 </div>
                 <div className="foody-container">
                     <h1>Recipes</h1>
                     <div>
-                    
-                    <p>We take pride in not just harvesting food, but making memorable meals.....view our recipes so you can wow your friends and family </p>
-               </div>
-               
+
+                        <p>We take pride in not just harvesting food, but making memorable meals.....view our recipes so you can wow your friends and family </p>
+                    </div>
+
                 </div>
             </div>
 
- 
-            
-            
+
+
+
         </div>
     )
 
